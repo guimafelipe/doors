ae552911b64f8852fbd6d53fc890390ef0901d75
Felipe Guimarães <Felipe Guimares>
2016-10-22 17:36:15 -0200

    =Did portal system, need to create folders per lvl

---
+++ Doors/Assets/PortalBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalBehaviour : MonoBehaviour {
+
+	public GameObject targetPortal;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	
+	void OnTriggerEnter(Collider other){
+		if(other.tag == "Player") {
+			other.transform.position = targetPortal.transform.position + targetPortal.transform.right*5 - targetPortal.transform.up*(-2);
+		}
+	}
+
+}

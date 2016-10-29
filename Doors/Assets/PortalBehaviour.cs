5120f28fd86ed5833baf2f4647990634df67e10e
Felipe Guimarães <Felipe Guimares>
2016-10-29 17:58:59 -0200

    Novo FPS controller

--- Doors/Assets/PortalBehaviour.cs
+++ Doors/Assets/PortalBehaviour.cs
@@ -19,6 +19,14 @@ public class PortalBehaviour : MonoBehaviour {
 	void OnTriggerEnter(Collider other){
 		if(other.tag == "Player") {
 			other.transform.position = targetPortal.transform.position + targetPortal.transform.right*5 - targetPortal.transform.up*(-2);
+			//other.transform.localEulerAngles = new Vector3(0f,90f,0f);
+			other.transform.rotation = targetPortal.transform.rotation;
+			/*other.transform.eulerAngles = new Vector3(
+				targetPortal.transform.eulerAngles.x,
+				targetPortal.transform.eulerAngles.y + 90,
+				targetPortal.transform.eulerAngles.z
+			);*/
+			other.transform.Rotate (0f, 90f, 0f);
 		}
 	}
 

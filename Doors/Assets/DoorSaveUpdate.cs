d910ceed6dc7c2265ee5eada97b519fc5d34e95d
Felipe Guimarães <Felipe Guimares>
2016-11-02 19:15:43 -0200

    implementaçao do codigo de save

---
+++ Doors/Assets/DoorSaveUpdate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorSaveUpdate : MonoBehaviour {
+
+	public int doorLevel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.tag == "Player") {
+			saveScript.saveObject.higherLevelReached = Mathf.Max(doorLevel,saveScript.saveObject.higherLevelReached);
+		}
+	}
+}
+
---
+++ Doors/Assets/saveScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class saveScript : MonoBehaviour {
+
+	public static saveScript saveObject;
+	public int higherLevelReached = 1;
+
+	// Use this for initialization
+	void Awake () {
+		if (saveObject == null) {
+			DontDestroyOnLoad (gameObject);
+			saveObject = this;
+		} else if (saveObject != this) {
+			Destroy (gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Save () {
+	
+	}
+
+	void Load(){
+	}
+
+}
